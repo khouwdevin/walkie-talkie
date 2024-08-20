f59912d8a91f8960092782bfdb0e4c2a03670010
khouwdevin <khouwdevin@gmail.com>
2024-08-20 21:44:53 +0700

    update(client): change random bytes to 8

--- walkie-talkie-client/app/api/room/route.ts
+++ walkie-talkie-client/app/api/room/route.ts
@@ -4,22 +4,22 @@ import crypto from "crypto"
 
 export async function POST() {
     try {
-        const username = crypto.randomBytes(48).toString("hex")
-        const roomName = "walkie-talkie"
-  
-        const at = new AccessToken(
-          process.env.LIVEKIT_API_KEY,
-          process.env.LIVEKIT_API_SECRET,
-          {
-            identity: username,
-          },
-        )
-        at.addGrant({ roomJoin: true, room: roomName })
-  
-        const token = await at.toJwt()
-  
-        return NextResponse.json({ token })
-    } catch (e) {
-        return NextResponse.json({ error: e })
+      const username = crypto.randomBytes(8).toString("hex")
+      const roomName = "walkie-talkie"
+
+      const at = new AccessToken(
+        process.env.LIVEKIT_API_KEY,
+        process.env.LIVEKIT_API_SECRET,
+        {
+          identity: username,
+        },
+      )
+      at.addGrant({ roomJoin: true, room: roomName })
+
+      const token = await at.toJwt()
+
+      return NextResponse.json({ token })
+    } catch(e) {
+      return NextResponse.json({ error: e })
     }
 }
